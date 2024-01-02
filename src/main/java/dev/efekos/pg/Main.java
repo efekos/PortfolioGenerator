dc5647899a2f0d84e4177f34a45ea53661618174
efekos <hefeolgun10@gmail.com>
2024-01-02 18:26:00 +0300

    add #getMainPath

--- src/main/java/dev/efekos/pg/Main.java
+++ src/main/java/dev/efekos/pg/Main.java
@@ -1,7 +1,13 @@
 package dev.efekos.pg;
 
 public class Main {
+    private static String MAIN_PATH;
+
+    public static String getMainPath() {
+        return MAIN_PATH;
+    }
+
     public static void main(String[] args) {
-        System.out.println(args);
+        MAIN_PATH = System.getProperty("user.dir");
     }
 }
