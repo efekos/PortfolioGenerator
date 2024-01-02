b02f3bd9985b7b962dae53ffc9c6e0cca8f2aae4
efekos <hefeolgun10@gmail.com>
2024-01-02 16:53:12 +0300

    move main class to a pg package

--- src/main/java/dev/efekos/pg/Main.java
+++ src/main/java/dev/efekos/pg/Main.java
@@ -1,7 +1,7 @@
-package dev.efekos;
+package dev.efekos.pg;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(args);
     }
 }
