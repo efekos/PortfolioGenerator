4045001cd9510bcccb779508110f617d64c7005f
efekos <hefeolgun10@gmail.com>
2024-01-03 19:09:17 +0300

    add #readJson

--- src/main/java/dev/efekos/pg/data/schema/DayDate.java
+++ src/main/java/dev/efekos/pg/data/schema/DayDate.java
@@ -1,6 +1,11 @@
 package dev.efekos.pg.data.schema;
 
-public class DayDate {
+import com.google.gson.JsonObject;
+import dev.efekos.pg.data.DataGrabberContext;
+import dev.efekos.pg.data.type.DataTypeChecker;
+import dev.efekos.pg.data.type.RequiredDataType;
+
+public class DayDate implements JsonSchema{
     private int day;
     private int month;
     private int year;
@@ -33,5 +38,16 @@ public class DayDate {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    public void readJson(JsonObject object, DataGrabberContext context) {
+        DataTypeChecker checker = new DataTypeChecker(context.getCurrentFile());
+        checker.searchExceptions(object,"year", RequiredDataType.INTEGER);
+        checker.searchExceptions(object,"month", RequiredDataType.INTEGER);
+        checker.searchExceptions(object,"day", RequiredDataType.INTEGER);
+
+        this.day = object.get("day").getAsInt();
+        this.year = object.get("year").getAsInt();
+        this.month = object.get("month").getAsInt();
     }
 }
