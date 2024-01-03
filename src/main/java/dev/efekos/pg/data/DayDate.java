3228b50541d3fc642c9ae824890dffa8922407df
efekos <hefeolgun10@gmail.com>
2024-01-03 18:26:59 +0300

    add dates

---
+++ src/main/java/dev/efekos/pg/data/DayDate.java
@@ -0,0 +1,37 @@
+package dev.efekos.pg.data;
+
+public class DayDate {
+    private int day;
+    private int month;
+    private int year;
+
+    public DayDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ src/main/java/dev/efekos/pg/data/FullDate.java
@@ -0,0 +1,67 @@
+package dev.efekos.pg.data;
+
+public class FullDate {
+    private int day;
+    private int month;
+    private int year;
+    private int minute;
+    private int hour;
+    private int second;
+
+    public FullDate(int day, int month, int year, int minute, int hour, int second) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.minute = minute;
+        this.hour = hour;
+        this.second = second;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+}
