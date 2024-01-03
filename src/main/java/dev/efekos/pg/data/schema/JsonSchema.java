6c5579844df4161c8ae916b09cdf7c74897f9a65
efekos <hefeolgun10@gmail.com>
2024-01-03 19:09:24 +0300

    add context to parameters

--- src/main/java/dev/efekos/pg/data/schema/JsonSchema.java
+++ src/main/java/dev/efekos/pg/data/schema/JsonSchema.java
@@ -1,7 +1,8 @@
 package dev.efekos.pg.data.schema;
 
 import com.google.gson.JsonObject;
+import dev.efekos.pg.data.DataGrabberContext;
 
 public interface JsonSchema {
-    void readJson(JsonObject object);
+    void readJson(JsonObject object, DataGrabberContext context);
 }
