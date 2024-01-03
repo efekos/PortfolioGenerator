31a1d3277c9aa452572a157dc0878316c558de7f
efekos <hefeolgun10@gmail.com>
2024-01-03 18:51:30 +0300

    add generalInfo grabbing

--- src/main/java/dev/efekos/pg/Main.java
+++ src/main/java/dev/efekos/pg/Main.java
@@ -1,13 +1,22 @@
 package dev.efekos.pg;
 
+import dev.efekos.pg.data.DataGrabber;
+import dev.efekos.pg.data.schema.GeneralInfo;
+
+import java.nio.file.Path;
+
 public class Main {
     private static String MAIN_PATH;
 
-    public static String getMainPath() {
-        return MAIN_PATH;
+    public static Path getMainPath() {
+        return Path.of(MAIN_PATH);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
         MAIN_PATH = System.getProperty("user.dir");
+
+        DataGrabber grabber = new DataGrabber(MAIN_PATH);
+
+        GeneralInfo generalInfo = grabber.grabGeneralInfo();
     }
 }
