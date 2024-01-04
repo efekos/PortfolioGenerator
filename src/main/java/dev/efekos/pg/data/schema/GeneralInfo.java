bb069eb9153d711a7a32814dce3e0cf6f11296a0
efekos <hefeolgun10@gmail.com>
2024-01-04 07:58:21 +0300

    switch to DataTypeChecker logic

--- src/main/java/dev/efekos/pg/data/schema/GeneralInfo.java
+++ src/main/java/dev/efekos/pg/data/schema/GeneralInfo.java
@@ -4,11 +4,18 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
 import dev.efekos.pg.data.DataGrabberContext;
+import dev.efekos.pg.data.type.DataTypeChecker;
+import dev.efekos.pg.data.type.RequiredDataType;
+
+import java.util.List;
+import java.util.Locale;
 
 public class GeneralInfo implements JsonSchema{
     private String name;
     private DayDate birthDate;
     private String title;
+    private Locale nativeLanguage;
+    private List<Locale> knownLanguages;
 
     public GeneralInfo(String name, DayDate birthDate, String title) {
         this.name = name;
@@ -25,16 +32,22 @@ public class GeneralInfo implements JsonSchema{
 
     @Override
     public void readJson(JsonObject object, DataGrabberContext context) {
-        if(!object.has("name")) throw new JsonSyntaxException("'name' missing in file 'general.json'");
-        if(!object.has("title")) throw new JsonSyntaxException("'title' missing in file 'general.json'");
-        if(!object.has("birth")) throw new JsonSyntaxException("'birth' missing in file 'general.json'");
+        DataTypeChecker checker = new DataTypeChecker();
+        checker.setCurrentFile(context.getCurrentFile());
 
+        checker.searchExceptions(object,"name", RequiredDataType.STRING);
+        checker.searchExceptions(object,"title", RequiredDataType.STRING);
+        checker.searchExceptions(object,"birth", RequiredDataType.OBJECT);
+
+        // name, title
         this.name = object.get("name").getAsString();
         this.title = object.get("title").getAsString();
+
+        // birth
         this.birthDate = new DayDate(26,2,2010);
-        JsonElement element = object.get("birth");
-        if(!element.isJsonObject()) throw new JsonSyntaxException("'birth' must be an object.");
-        birthDate.readJson(element.getAsJsonObject(),context);
+        birthDate.readJson(object.get("birth").getAsJsonObject(),context);
+
+
     }
 
     public void setName(String name) {
