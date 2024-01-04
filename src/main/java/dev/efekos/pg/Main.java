e147e5d91a6864990178d7a5e6ab3770dc7ddb65
efekos <hefeolgun10@gmail.com>
2024-01-04 21:24:18 +0300

    add generator logic

--- src/main/java/dev/efekos/pg/Main.java
+++ src/main/java/dev/efekos/pg/Main.java
@@ -2,7 +2,11 @@ package dev.efekos.pg;
 
 import dev.efekos.pg.data.DataGrabber;
 import dev.efekos.pg.data.schema.GeneralInfo;
+import dev.efekos.pg.output.FileGenerator;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main {
@@ -17,6 +21,23 @@ public class Main {
 
         DataGrabber grabber = new DataGrabber(MAIN_PATH);
 
+        // data grabbing
         GeneralInfo generalInfo = grabber.grabGeneralInfo();
+
+        // bin
+        String binPath = MAIN_PATH+"\\bin";
+        Path path = Path.of(binPath);
+        Files.deleteIfExists(path);
+
+        // generating
+        FileGenerator generator = new FileGenerator();
+        Files.createDirectory(path);
+
+        generator.generateIndexFile(generalInfo,binPath);
+    }
+
+    public static String readStringResource(String path) throws IOException {
+        InputStream stream = Main.class.getResource(path).openStream();
+        return new String(stream.readAllBytes());
     }
 }
