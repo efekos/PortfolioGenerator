b4afc744ac2cdd7769a55da92c02be974d8267cf
efekos <hefeolgun10@gmail.com>
2024-01-06 15:31:43 +0300

    add MonthDate

---
+++ src/main/java/dev/efekos/pg/data/schema/MonthDate.java
@@ -0,0 +1,44 @@
+package dev.efekos.pg.data.schema;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import dev.efekos.pg.data.DataGrabberContext;
+import dev.efekos.pg.data.type.DataTypeChecker;
+import dev.efekos.pg.data.type.RequiredDataType;
+
+public class MonthDate implements JsonSchema{
+    private int month;
+    private int year;
+
+    public MonthDate(int month, int year) {
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public void readJson(JsonObject object, DataGrabberContext context) throws JsonParseException {
+        DataTypeChecker checker = new DataTypeChecker(context.getCurrentFile());
+
+        checker.searchExceptions(object,"month", RequiredDataType.INTEGER);
+        checker.searchExceptions(object,"year", RequiredDataType.INTEGER);
+
+        this.month = object.get("month").getAsInt();
+        this.year = object.get("year").getAsInt();
+    }
+}
