ccde2721b3bcb63f8e3bc3ae60ba84ad9af2a515
efekos <hefeolgun10@gmail.com>
2024-01-06 13:07:10 +0300

    add JavaDoc

--- src/main/java/dev/efekos/pg/data/schema/DayDate.java
+++ src/main/java/dev/efekos/pg/data/schema/DayDate.java
@@ -5,40 +5,83 @@ import dev.efekos.pg.data.DataGrabberContext;
 import dev.efekos.pg.data.type.DataTypeChecker;
 import dev.efekos.pg.data.type.RequiredDataType;
 
+/**
+ * Represents a date that just needs a day, year, and month.
+ */
 public class DayDate implements JsonSchema{
+    /**
+     * Day of the date. Will be day of the month (such as 2nd July, but not Tuesday)
+     */
     private int day;
+    /**
+     * Month of the date.
+     */
     private int month;
+    /**
+     * Year of the date.
+     */
     private int year;
 
+    /**
+     * Constructs a new {@link DayDate}
+     * @param day Day of this date. Will be considered as day of the month (such as 2nd July, but not Tuesday).
+     * @param month Month of this date.
+     * @param year Year of this date.
+     */
     public DayDate(int day, int month, int year) {
         this.day = day;
         this.month = month;
         this.year = year;
     }
 
+    /**
+     * Returns the day of this date
+     * @return Day value of this date
+     */
     public int getDay() {
         return day;
     }
 
+    /**
+     * Changes the day of this date
+     * @param day New day value
+     */
     public void setDay(int day) {
         this.day = day;
     }
 
+    /**
+     * Returns month value of this date.
+     * @return Month of this date.
+     */
     public int getMonth() {
         return month;
     }
 
+    /**
+     * Changes the month of this date.
+     * @param month Month of this date.
+     */
     public void setMonth(int month) {
         this.month = month;
     }
 
+    /**
+     * Returns the year of this date.
+     * @return Year value of this date.
+     */
     public int getYear() {
         return year;
     }
 
+    /**
+     * Changes the year value of this date.
+     * @param year New year value.
+     */
     public void setYear(int year) {
         this.year = year;
     }
+
 
     public void readJson(JsonObject object, DataGrabberContext context) {
         DataTypeChecker checker = new DataTypeChecker(context.getCurrentFile());
--- src/main/java/dev/efekos/pg/data/schema/JsonSchema.java
+++ src/main/java/dev/efekos/pg/data/schema/JsonSchema.java
@@ -1,8 +1,15 @@
 package dev.efekos.pg.data.schema;
 
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import dev.efekos.pg.data.DataGrabberContext;
 
 public interface JsonSchema {
-    void readJson(JsonObject object, DataGrabberContext context);
+    /**
+     * Reads values from the object given.
+     * @param object A {@link JsonObject}.
+     * @param context Current grabbing context for exception messages
+     * @throws JsonParseException Depending on what class is this.
+     */
+    void readJson(JsonObject object, DataGrabberContext context) throws JsonParseException;
 }
