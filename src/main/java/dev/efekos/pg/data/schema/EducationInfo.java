1084a911e00fbd77e4f83b37cbe8f58fcca50ae3
efekos <hefeolgun10@gmail.com>
2024-01-06 15:44:14 +0300

    add .entries

--- src/main/java/dev/efekos/pg/data/schema/EducationInfo.java
+++ src/main/java/dev/efekos/pg/data/schema/EducationInfo.java
@@ -1,4 +1,45 @@
 package dev.efekos.pg.data.schema;
 
-public class EducationInfo {
+import com.google.gson.*;
+import dev.efekos.pg.data.DataGrabberContext;
+import dev.efekos.pg.data.type.DataTypeChecker;
+import dev.efekos.pg.data.type.RequiredDataType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EducationInfo implements JsonSchema{
+    private List<EducationEntry> entries;
+
+    public EducationInfo(List<EducationEntry> entries) {
+        this.entries = entries;
+    }
+
+    public EducationInfo() {
+    }
+
+    public List<EducationEntry> getEntries() {
+        return entries;
+    }
+
+    public void setEntries(List<EducationEntry> entries) {
+        this.entries = entries;
+    }
+
+    @Override
+    public void readJson(JsonObject object, DataGrabberContext context) throws JsonParseException {
+        DataTypeChecker checker = new DataTypeChecker();
+
+        checker.searchExceptions(object,"entries", RequiredDataType.ARRAY);
+
+        JsonArray array = object.get("entries").getAsJsonArray();
+        List<EducationEntry> entryList = new ArrayList<>();
+        for (JsonElement element : array) {
+            if(!element.isJsonObject()) throw new JsonSyntaxException("Elements in 'entries' must be an object at '"+context.getCurrentFile()+"'");
+            EducationEntry entry = new EducationEntry(EducationEntryIcon.SCHOOL, null, null, null, null);
+            entry.readJson(element.getAsJsonObject(),context);
+            entryList.add(entry);
+        }
+        this.entries = entryList;
+    }
 }
