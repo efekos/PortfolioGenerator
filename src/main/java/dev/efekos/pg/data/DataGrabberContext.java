fa71cd25f2bcbaec3f8dbc258d6ed7a6708aadd8
efekos <hefeolgun10@gmail.com>
2024-01-09 19:39:36 +0300

    remove parent file logics

--- src/main/java/dev/efekos/pg/data/DataGrabberContext.java
+++ src/main/java/dev/efekos/pg/data/DataGrabberContext.java
@@ -2,34 +2,16 @@ package dev.efekos.pg.data;
 
 public class DataGrabberContext {
     private String currentFile;
-    private boolean doesCurrentFileHaveParent;
-    private String currentParentFile;
 
     public DataGrabberContext(String currentFile) {
         this.currentFile = currentFile;
     }
 
-    public boolean doesCurrentFileHaveParent() {
-        return doesCurrentFileHaveParent;
-    }
-
-    public void setDoesCurrentFileHaveParent(boolean doesCurrentFileHaveParent) {
-        this.doesCurrentFileHaveParent = doesCurrentFileHaveParent;
-    }
-
-    public String getCurrentParentFile() {
-        return currentParentFile;
-    }
-
-    public void setCurrentParentFile(String currentParentFile) {
-        this.currentParentFile = currentParentFile;
+    public void setCurrentFile(String currentFile) {
+        this.currentFile = currentFile;
     }
 
     public String getCurrentFile() {
         return currentFile;
-    }
-
-    public void setCurrentFile(String currentFile) {
-        this.currentFile = currentFile;
     }
 }
