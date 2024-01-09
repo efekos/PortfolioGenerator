7ef85174c30606b91ea26b4fcdcfefb560d05a60
efekos <hefeolgun10@gmail.com>
2024-01-09 18:20:40 +0300

    add .doesCurrentFileHaveParent & currentParentFile

--- src/main/java/dev/efekos/pg/data/DataGrabberContext.java
+++ src/main/java/dev/efekos/pg/data/DataGrabberContext.java
@@ -2,9 +2,27 @@ package dev.efekos.pg.data;
 
 public class DataGrabberContext {
     private String currentFile;
+    private boolean doesCurrentFileHaveParent;
+    private String currentParentFile;
 
     public DataGrabberContext(String currentFile) {
         this.currentFile = currentFile;
+    }
+
+    public boolean doesCurrentFileHaveParent() {
+        return doesCurrentFileHaveParent;
+    }
+
+    public void setDoesCurrentFileHaveParent(boolean doesCurrentFileHaveParent) {
+        this.doesCurrentFileHaveParent = doesCurrentFileHaveParent;
+    }
+
+    public String getCurrentParentFile() {
+        return currentParentFile;
+    }
+
+    public void setCurrentParentFile(String currentParentFile) {
+        this.currentParentFile = currentParentFile;
     }
 
     public String getCurrentFile() {
