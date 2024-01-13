bdd05d9425327933bfbca404812d23e11aa12944
efekos <hefeolgun10@gmail.com>
2024-01-13 13:37:54 +0300

    put utility methods into an interface

---
+++ src/main/java/dev/efekos/pg/output/Generator.java
@@ -0,0 +1,44 @@
+package dev.efekos.pg.output;
+
+import dev.efekos.pg.Main;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Locale;
+
+public interface Generator {
+    default String makeId(String id){
+        return id
+                .replaceAll(" ","_")
+                .toLowerCase(Locale.ROOT);
+    }
+
+    default void copyResource(String resourceLocation, String outputLocation) throws IOException {
+        System.out.println("Copying file: " + resourceLocation);
+
+        String fileString = Main.readStringResource(resourceLocation);
+        File file = new File(binPath + outputLocation);
+        file.getParentFile().mkdirs();
+        file.createNewFile();
+
+        FileWriter writer = new FileWriter(file);
+        writer.write(fileString);
+        writer.flush();
+        writer.close();
+
+        System.out.println("Copied file: " + resourceLocation);
+    }
+
+    default void writeFile(String path, String content) throws IOException {
+        File file = new File(path);
+
+        file.createNewFile();
+
+        FileWriter writer = new FileWriter(file);
+
+        writer.write(content);
+        writer.flush();
+        writer.close();
+    }
+}
