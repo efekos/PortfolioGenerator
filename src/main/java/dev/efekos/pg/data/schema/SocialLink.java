041d8d6c183f467b400e32c51411bd1e90175954
efekos <hefeolgun10@gmail.com>
2024-01-15 23:26:31 +0300

    add getter setters

--- src/main/java/dev/efekos/pg/data/schema/SocialLink.java
+++ src/main/java/dev/efekos/pg/data/schema/SocialLink.java
@@ -1,12 +1,9 @@
 package dev.efekos.pg.data.schema;
 
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
 import dev.efekos.pg.data.DataGrabberContext;
 import dev.efekos.pg.data.type.DataTypeChecker;
 import dev.efekos.pg.data.type.RequiredDataType;
-
-import java.util.regex.Pattern;
 
 public class SocialLink implements JsonSchema {
     private String display;
@@ -28,5 +25,29 @@ public class SocialLink implements JsonSchema {
         this.display = object.get("display").getAsString();
         this.url = object.get("url").getAsString();
         this.icon = object.get("icon").getAsString();
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(String display) {
+        this.display = display;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
     }
 }
