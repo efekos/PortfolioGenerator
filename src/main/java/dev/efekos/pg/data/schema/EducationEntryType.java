0e946a46e8a29fe08875e14b57a5d664424f061a
efekos <hefeolgun10@gmail.com>
2024-01-17 20:28:53 +0300

    rename to EducationEntryType and add .display

---
+++ src/main/java/dev/efekos/pg/data/schema/EducationEntryType.java
@@ -0,0 +1,24 @@
+package dev.efekos.pg.data.schema;
+
+public enum EducationEntryType {
+    SCHOOL("school","School"),
+    UNIVERSITY("university","University"),
+    DEGREE("degree","Degree"),
+    COURSE("course","Course");
+
+    private final String id;
+    private final String display;
+
+    EducationEntryType(String id, String display) {
+        this.id = id;
+        this.display = display;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+}
