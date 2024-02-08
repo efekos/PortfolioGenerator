17704b4c76f516854a78ca4359db5a3fa850e678
efekos <hefeolgun10@gmail.com>
2024-02-08 21:28:51 +0300

    add all color keys

--- src/main/java/dev/efekos/pg/data/color/ColorTheme.java
+++ src/main/java/dev/efekos/pg/data/color/ColorTheme.java
@@ -17,8 +17,46 @@
 package dev.efekos.pg.data.color;
 
 public class ColorTheme {
+    public static final ColorGradient BLUE_BUTTON_COLOR = new ColorGradient("buttons.blueColor");
+    public static final ColorGradient GREEN_BUTTON_COLOR = new ColorGradient("buttons.greenColor");
+    public static final ColorGradient ORANGE_BUTTON_COLOR = new ColorGradient("buttons.orangeColor");
+    public static final ColorGradient RED_BUTTON_COLOR = new ColorGradient("buttons.redColor");
+    public static final Color DOWNLOAD_TEXT_COLOR = new Color("buttons.downloadTextColor");
+    public static final Color DOWNLOAD_TEXT_BACKGROUND_COLOR = new Color("buttons.downloadTextBackgroundColor");
+    public static final Color DOWNLOAD_TEXT_BACKGROUND_HOVER_COLOR = new Color("buttons.downloadTextBackgroundHoverColor");
+    public static final Color ENTRY_BACKGROUND_COLOR = new Color("entry.backgroundColor");
+    public static final Color ENTRY_BACKGROUND_HOVER_COLOR = new Color("entry.backgroundHoverColor");
+    public static final Color ENTRY_BORDER_COLOR = new Color("entry.borderColor");
+    public static final Color ENTRY_ALT_TEXT_COLOR = new Color("entry.altTextColor");
+    public static final Color ENTRY_ICON_BACKGROUND_COLOR = new Color("entry.iconBackgroundColor");
+    public static final Color ENTRY_ICON_COLOR = new Color("entry.iconColor");
+    public static final ColorGradient ENTRY_PLACEHOLDER_BACKGROUND_COLOR = new ColorGradient("entry.placeholderBackgroundColor");
     public static final Color GENERAL_BACKGROUND_COLOR = new Color("general.backgroundColor");
     public static final Color GENERAL_TEXT_COLOR = new Color("general.textColor");
-    public static final Color GENERAL_LINK_COLOR = new Color("general.linkColor");
+    public static final FontFamily GENERAL_FONT = new FontFamily("general.font");
+    public static final Font GENERAL_NAME_FONT = new Font("general.nameFont");
     public static final Color GENERAL_NAME_COLOR = new Color("general.nameColor");
+    public static final FontFamily GENERAL_TITLE_FONT = new FontFamily("general.titleFont");
+    public static final Color MARKDOWN_INLINE_CODE_BACKGROUND_COLOUR /*british alert!!!*/ = new Color("markdown.inlineCodeBackgroundColor");
+    public static final Color MARKDOWN_CONTENT_BORDER_COLOR = new Color("markdown.contentBorderColor");
+    public static final Color MARKDONW_CODE_BLOCK_BORDER_COLOR = new Color("markdown.codeBlockBorderColor");
+    public static final Color PROJECT_SEARCH_BAR_BACKGROUND_COLOR = new Color("projects.searchBarBackgroundColor");
+    public static final Color PROJECT_SEARCH_BAR_TEXT_COLOR = new Color("projects.searchBarTextColor");
+    public static final Color PROJECT_GALLERY_MODAL_BACKGROUND_COLOR = new Color("projects.galleryModalBackgroundColor");
+    public static final Color PROJECT_GALLERY_MODAL_HEADER_BACKGROUND_COLOR = new Color("projects.galleryModalHeaderBackgroundColor");
+    public static final Color PROJECT_GALLERY_MODAL_FOOTER_BACKGROUND__COLOR = new Color("projects.galleryModalFooterBackgroundColor");
+    public static final Color PROJECT_GALLERY_IMAGE_BORDER_COLOR = new Color("projects.galleryImageBorderColor");
+    public static final Color PROJECT_GALLERY_IMAGE_BACKGROUND_COLOR = new Color("projects.galleryImageBackgroundColor");
+    public static final Color PROJECT_NAVIGATION_BAR_COLOR = new Color("projects.navigationBarColor");
+    public static final Color PROJECT_NAVIGATION_BAR_SELECTED_COLOR = new Color("projects.navigationBarSelectedColor");
+    public static final Color PROJECT_PAGINATION_BUTTON_COLOR = new Color("projects.paginationButtonColor");
+    public static final Color PROJECT_PAGINATION_DISABLED_BUTTON_COLOR = new Color("projects.paginationDisabledButtonColor");
+    public static final Color PROJECT_LINK_COLOR = new Color("projects.projectLinkColor");
+    public static final Color PROJECT_SUMMARY_TEXT_COLOR = new Color("projects.summaryTextColor");
+    public static final Color PROJECT_VERSION_TAG_RELEASE = new Color("projects.versionTags.releaseColor");
+    public static final Color PROJECT_VERSION_TAG_BETA = new Color("projects.versionTags.betaColor");
+    public static final Color PROJECT_VERSION_TAG_ALPHA = new Color("projects.versionTags.alphaColor");
+    public static final Color PROJECT_VERSION_TAG_PROTOTYPE = new Color("projects.versionTags.prototypeColor");
+    public static final Color PROJECT_VERSION_TAG_RC = new Color("projects.versionTags.releaseCandidateColor");
+    public static final Color PROJECT_VERSION_TAG_PRE_RELEASE = new Color("projects.versionTags.preReleaseColor");
 }
