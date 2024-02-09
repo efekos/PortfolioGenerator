dc75ab8ff215c0cb8a69ed0b0d453101cd44988e
efekos <hefeolgun10@gmail.com>
2024-02-09 18:15:23 +0300

    change colors to keys

--- src/main/java/dev/efekos/pg/data/type/VersionType.java
+++ src/main/java/dev/efekos/pg/data/type/VersionType.java
@@ -17,13 +17,13 @@
 package dev.efekos.pg.data.type;
 
 public enum VersionType {
-    RELEASE("release","Release","rgb(99, 224, 118)"),
-    BETA("beta","Beta","rgb(255, 111, 0)"),
-    ALPHA("alpha","Alpha","rgb(255, 59, 20)"),
-    PROTOTYPE("prototype","Prototype","rgb(64, 119, 230)"),
-    SNAPSHOT("snapshot","Snapshot","rgb(245, 240, 93)"),
-    RELEASE_CANDIDATE("rc","Release Candidate","rgb(130, 219, 94)"),
-    PRE_RELEASE("pre_release","Pre-Release","rgb(110, 219, 66)")
+    RELEASE("release","Release","releaseColor"),
+    BETA("beta","Beta","betaColor"),
+    ALPHA("alpha","Alpha","alphaColor"),
+    PROTOTYPE("prototype","Prototype","prototypeColor"),
+    SNAPSHOT("snapshot","Snapshot","snapshotColor"),
+    RELEASE_CANDIDATE("rc","Release Candidate","releaseCandidateColor"),
+    PRE_RELEASE("pre_release","Pre-Release","preReleaseColor")
     ;
 
     private final String id;
