2f9b9867f17309b76068efb79aecc37408086abd
efekos <hefeolgun10@gmail.com>
2024-02-22 18:33:16 +0300

    Change language finder script

--- src/main/resources/site/script/language_finder.js
+++ src/main/resources/site/script/language_finder.js
@@ -13,19 +13,69 @@
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-const id = "%%UUID%%";
+const idddd = "%%UUID%%";
 
-const url = "https://raw.githubusercontent.com/efekos/PortfolioGenerator/master/src/main/resources/lang/%LANG%.json";
+const url = "./lang/%LANG%.json";
 
 var currentLang = {};
 
-function loadLang() {
-    const lang = localStorage.getItem(`pgen-lang-${id}`) ?? 'en';
-    fetch(url.replace("%LANG%", lang)).then(res => res.json()).then(res => { currentLang = res; console.log("Successfully loaded language!")});
+async function loadLang() {
+    const lang = localStorage.getItem(`pgen-lang-${idddd}`) ?? 'en';
+    const res = await fetch(url.replace("%LANG%", lang)).then(res => res.json());
+
+    currentLang = res;
+    console.log("Successfully loaded language!")
 }
 
-function refreshLang(code) {
-    localStorage.setItem(`pgen-lang-${id}`, code);
+function getKey(key,...args){
+    if(!key in currentLang) throw Error("Unknown translation key: "+key);
+
+
+    var final = currentLang[key];
+
+    for (let i = 0; i < args.length; i++) {
+        const element = args[i];
+
+        final = final.replace(`{${i}}`,element);
+    }
+
+    return final;
 }
 
-loadLang();
+async function refreshLang(code) {
+    await localStorage.setItem(`pgen-lang-${idddd}`, code);
+    await loadLang();
+    refreshLangKeys();
+}
+
+function refreshLangKeys(){
+    const keys = document.getElementsByClassName("key");
+
+    for (let i = 0; i < keys.length; i++) {
+        const element = keys.item(i);
+
+        if(element.id.startsWith("key-")){
+            const key = element.id.substring(4)
+            const arguments = findArguments(element);
+
+            element.innerHTML = getKey(key,...arguments);
+        }
+
+    }
+}
+
+function findArguments(element){
+    const argumentAttributes = element.getAttributeNames().filter(r=>/arg[0-9]+/i.test(r))
+
+    let list = [];
+
+    argumentAttributes.forEach(s=>{
+        list = [...list,element.getAttribute(s)]
+    })
+
+    return list;
+}
+
+loadLang().then(()=>{
+    refreshLangKeys();
+});
